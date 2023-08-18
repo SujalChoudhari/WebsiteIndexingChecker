18384fe7162d26eedf85c9bae1ac8cd7be89a970
Sujal Choudhari <sjlchoudhari@gmail.com>
2023-08-18 12:33:19 +0530

    Timeout handled

--- app.py
+++ app.py
@@ -9,6 +9,13 @@ from src.constants import URL_TO_SHEETS, SERVICE_ACCOUNT_FILENAME
 
 app = flask.Flask(__name__)
 
+
+proxy_manager = None
+spreadsheet_manager = None
+url_manager = None
+index_checker = None
+fail_count = 0
+
 @app.route("/",methods=['GET','POST'])
 def index():
     """
@@ -18,19 +25,14 @@ def index():
     return flask.render_template("index.html", url_to_sheets=URL_TO_SHEETS)
 
 
-
-@app.route("/check", methods=["POST"])
-def check():
+@app.route("/load-proxy", methods=["POST"])
+def load_proxy():
     """
         The Main function that is ran to check the urls.
         This route is called by the UI when the user clicks the "Start" button
         Renders a done page when finished, with a message, or an error page if something went wrong
     """
-    """
-        Main Runner function called when user presses `Start` button.
-        This organizes the flow of the program
-        Checks for any errors and updates the progress manager
-    """
+    global proxy_manager
     try:
         # pull proxies from text file
         ProgressManager.update_progress("Starting App ...")
@@ -40,8 +42,13 @@ def check():
     except Exception as e:
         print(e)
         ProgressManager.update_progress("Failed to load proxies: " + str(e),False)
-        return flask.render_template("index.html", url_to_sheets=URL_TO_SHEETS, error=f"Failed to load proxies: {e}")
 
+    return flask.redirect(flask.url_for('load_sheets'))
+
+
+@app.route('/load-sheet')
+def load_sheets():
+    global spreadsheet_manager,url_manager
     try:
         # pull sitemaps from the spreadsheet
         ProgressManager.update_progress("Loading Spreadsheet ...")
@@ -54,20 +61,32 @@ def check():
     except Exception as e:
         print(e)
         ProgressManager.update_progress("Failed to load spreadsheet: " + str(e),False)
-        return flask.render_template("index.html", url_to_sheets=URL_TO_SHEETS, error=f"Failed to load spreadsheet: {e}")
+    return flask.redirect(flask.url_for('check_index'))
 
+@app.route('/check-index')
+def check_index():
+    global index_checker,fail_count
     try:
         # run the checks
-        index_checker = Indexer(proxy_manager, url_manager, spreadsheet_manager)
-        index_checker.process()
-        ProgressManager.update_progress("Checks Complete")
+        if index_checker == None:
+            index_checker = Indexer(proxy_manager, url_manager, spreadsheet_manager)
 
+        if fail_count > 10:
+            raise Exception("Failed more than 10 times")
+        
+        if url_manager.has_more_urls():
+            url, is_indexed, status = index_checker.process()
+            if status == 'failed':
+                fail_count +=1
+            
+            return flask.redirect(flask.url_for('check_index'))
+        
+        ProgressManager.update_progress("Checks Complete")
         # save unindexed
         spreadsheet_manager.save_unindexed_to_sheets()
     except Exception as e:
         print(e)
         ProgressManager.update_progress("Failed to run checks: " + str(e),False)
-        return flask.render_template("index.html", url_to_sheets=URL_TO_SHEETS, error=f"Failed to run checks: {e}")
 
     ProgressManager.update_progress("Done!", is_working=True)
     return flask.render_template("done.html", message=ProgressManager.done_message)
--- src/indexer.py
+++ src/indexer.py
@@ -9,6 +9,7 @@ from src.proxy_manager import ProxyManager
 from src.progress_manager import ProgressManager
 from src.constants import INDEXING_SEARCH_STRING
 
+
 class Indexer:
     """
     # Indexer
@@ -27,6 +28,7 @@ class Indexer:
         - If the proxy manager runs out of proxies, mark the url as failed in the url manager
         - If more than 5 urls fail in a row, stop the process, exit
     """
+
     def __init__(
         self,
         proxy_manager: ProxyManager,
@@ -40,34 +42,27 @@ class Indexer:
         self.current_proxy = None
 
     def process(self):
-        fail_count = 0
-        while self.url_manager.has_more_urls():
-            ProgressManager.update_progress("Progress: {}/{}, Consistent Fails: {}".format(
-                self.url_manager.current_url_index,
-                len(self.url_manager.urls),
-                fail_count
-            ))
+        if self.url_manager.has_more_urls():
+            ProgressManager.update_progress(
+                "Progress: {}/{}".format(
+                    self.url_manager.current_url_index,
+                    len(self.url_manager.urls),
+                )
+            )
 
             url, is_indexed, status = self.check_next_url()
-            print("URL " + url + " is indexed: " + str(is_indexed) + " Status: " + status)
+            print(
+                "URL " + url + " is indexed: " + str(is_indexed) + " Status: " + status
+            )
             if not is_indexed and status == "checked":
                 self.sheet_manager.add_unindexed_url(url)
 
-            if status == "end":
-                break
-            elif status == "checked":
-                fail_count = 0
-            elif status == "failed":
-                fail_count += 1
-            
-            if fail_count > 5:
-                ProgressManager.update_progress("Failed consistently more than 5 times! Exiting Process...")
-                ProgressManager.done_message = "Failed consistently 5 times! Exiting Process..."
-                return
-            
+
             if self.url_manager.current_url_index % 50 == 0:
                 ProgressManager.update_progress("Saving unindexed urls to sheets...")
                 self.sheet_manager.save_unindexed_to_sheets()
+
+            return url, is_indexed, status
 
     def check_next_url(self):
         current_url = self.url_manager.get_next_url()
@@ -82,7 +77,7 @@ class Indexer:
                 return current_url, False, "checked"
             else:
                 return current_url, True, "checked"
-            
+
         except Exception as e:
             print("Error: ", e)
             return current_url, False, "failed"
@@ -93,7 +88,7 @@ class Indexer:
             current_proxy = self.proxy_manager.get_proxy_for_request()
 
             if current_proxy is None:
-                ProgressManager.update_progress("All given proxy failed")
+                ProgressManager.update_progress("All given proxy failed, using regular requests")
                 return requests.get(url, **kwargs)
 
             print("\n\nUsing Proxy: ", current_proxy["http"])
@@ -112,12 +107,14 @@ class Indexer:
                 print("\tFailed!")
                 fail_count += 1
                 self.proxy_manager.update_proxy()
-            
+
             if fail_count > 5:
                 break
 
         print("No proxies left!")
-        ProgressManager.update_progress("No working proxies left! Using normal request...")
+        ProgressManager.update_progress(
+            "No working proxies left! Using normal request..."
+        )
         self.proxy_manager.update_proxy()
         response = requests.get(url, **kwargs)
         if response.status_code != 200:
