33a0fd3a7b75d5e88f18ce8ba69353c4a90ef651
Sujal Choudhari <sjlchoudhari@gmail.com>
2023-08-21 14:01:03 +0530

    language forced to english

--- src/constants.py
+++ src/constants.py
@@ -3,5 +3,6 @@ URL_TO_SHEETS = os.environ.get("URL_TO_SHEETS","https://docs.google.com/spreadsh
 SERVICE_ACCOUNT_FILENAME = os.environ.get("SERVICE_ACCOUNT_FILENAME","service_account.json")
 INDEXING_SEARCH_STRING = "https://www.google.com/search?q=site:{}&num=1"
 REQUEST_HEADERS = {
-    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.135 Safari/537.36 Edge/12.246"
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.135 Safari/537.36 Edge/12.246",
+    "Accept-Language": "en-US,en;q=0.9",
 }
--- src/indexer.py
+++ src/indexer.py
@@ -59,6 +59,7 @@ class Indexer:
                 fail_count = 0
             elif status == "failed":
                 fail_count += 1
+
             
             if fail_count > 5:
                 ProgressManager.update_progress("Failed consistently more than 5 times! Exiting Process...")
@@ -115,7 +116,7 @@ class Indexer:
                 print("Failed!", e)
                 fail_count += 1
                 self.proxy_manager.update_proxy()
-                ProgressManager.update_progress(f"Request failed! {e.__class__.__name__}, ")
+                ProgressManager.update_progress(f"Request failed! {e.__class__.__name__}. ")
                 break
         time.sleep(5)
-        return requests.get(url,timeout=20)
+        return requests.get(url,timeout=20,headers=REQUEST_HEADERS)
